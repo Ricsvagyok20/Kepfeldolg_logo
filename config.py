dac2701a915f433d18e5d064c7340666157c01a6
kqby <qb3tty@gmail.com>
2024-11-09 14:57:34 +0100

    nike logo pictures update

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import glob
 LOGO_PATHS = [
     glob.glob('assets/logos/honda/*.jpg'),
     glob.glob('assets/logos/apple/preproc/*.jpg'),
-    glob.glob('assets/logos/nike/preproc/*.jpg'),
+    glob.glob('assets/logos/nike/*.jpg'),
     glob.glob('assets/logos/peugeot/*.jpg'),
     glob.glob('assets/logos/logitech/*.jpg')
 ]
