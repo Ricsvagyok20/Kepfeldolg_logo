e0e03e5ad06f4c6e355dbc5592226a07ee1b713a
Rück Richárd <h163500@stud.u-szeged.hu>
2024-11-29 21:18:16 +0100

    Added new images for template creation for honda and peugeot logos. Edited the paths in config. Made the matched_region rectangle in chamfer-matching bigger by 10 pixels on each side so if the best template was a little smaller the image still fully captures the logo.

--- config.py
+++ config.py
@@ -4,18 +4,18 @@ import glob
 LOGO_PATHS_WITH_TEMPLATES = [
     {
         'logo_paths': glob.glob('assets/logos/honda/*.jpg'),
-        'template_path': 'assets/logos/honda/honda_logo_main_for_chamfer.jpg'
+        'template_path': 'assets/logos/honda/honda_logo_main.jpg'
     },
-    # {
-    #     'logo_paths': glob.glob('assets/logos/apple/*.jpg'),
-    #     'template_path': 'assets/logos/apple/apple_pure.jpg'
-    # },
-    # {
-    #     'logo_paths': glob.glob('assets/logos/nike/*.jpg'),
-    #     'template_path': 'assets/logos/nike/nike_logo_pure.jpg'
-    # },
-    # {
-    #     'logo_paths': glob.glob('assets/logos/peugeot/*.jpg'),
-    #     'template_path': 'assets/logos/peugeot/peugeot_logo_23.jpg'
-    # },
+    {
+        'logo_paths': glob.glob('assets/logos/apple/*.jpg'),
+        'template_path': 'assets/logos/apple/apple_pure.jpg'
+    },
+    {
+        'logo_paths': glob.glob('assets/logos/nike/*.jpg'),
+        'template_path': 'assets/logos/nike/nike_logo_pure.jpg'
+    },
+    {
+        'logo_paths': glob.glob('assets/logos/peugeot/*.jpg'),
+        'template_path': 'assets/logos/peugeot/peugeot_logo_main.jpg'
+    },
 ]
