3858e0386e7e79b42e017238e27ede96cf94bbb6
Lilla Bajnai <h042724@stud.u-szeged.hu>
2024-12-01 21:51:54 +0100

    restored main to chamfer matching and added accuracy calculating and chart visualising methods

--- main.py
+++ main.py
@@ -20,37 +20,32 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from keypoint_detection.keypointDetection import predict_with_keypoint
 
 # Fő program
-image_paths = []
 original_images = []
+image_paths = []
 processed_images = []
-images = []
+
+# for i in glob.glob('processed_images/*.jpg'):
+#     image = cv.imread(i)
+#     image_paths.append(i)
+#
+#     original_images.append(image)
+#
+# normalized_images = np.array(original_images)
+#
+# processed_images = normalized_images / 255.0
 
 # Iterate through each logo path and its corresponding template
-for i in glob.glob('processed_images/*.jpg'):
-    image_paths.append(i)
-    image = cv.imread(i)
+for logo_group in config.LOGO_PATHS_WITH_TEMPLATES:
+    logo_paths = logo_group['logo_paths']
+    image_paths.extend(logo_paths)
+    template_path = logo_group['template_path']
 
-    original_images.append(image)
+    # Process images for the current logo group
+    group_processed_images, original_image_group = process_images(logo_paths, template_path)
+    original_images.extend(original_image_group)
+    processed_images.append(group_processed_images)
 
-normalized_images = np.array(original_images)
-
-normalized_images = normalized_images / 255.0
-
-processed_images = np.concatenate(normalized_images, axis=0)
-
-counter = 0
-
-# for logo_group in config.LOGO_PATHS_WITH_TEMPLATES:
-#     logo_paths = logo_group['logo_paths']
-#     image_paths.extend(logo_paths)
-#     template_path = logo_group['template_path']
-#
-#     # Process images for the current logo group
-#     group_processed_images, images = process_images(logo_paths, template_path)
-#     for i in range(len(images)):
-#         filename = 'processed_images/processed_image_' + str(counter) + '.jpg'
-#         cv.imwrite(filename, images[i])
-#         counter += 1
+processed_images = np.concatenate(processed_images, axis=0)
 
 # Címkék generálása
 labels = []
@@ -62,56 +57,14 @@ labels = np.array(labels)
 
 label_dict = { 0: 'Honda', 1: 'Apple', 2: 'Nike', 3: 'Peugeot' }
 
-# Keypoint detection accuracy mérés
-# labels_keypoint = []
-# for image in original_images:
-#     prediction, image_2 = predict_with_keypoint(image)
-#     labels_keypoint.append(prediction)
-#
-# keypoint_accuracy = 0
-# print(len(labels), len(labels_keypoint))
-# for i, label in enumerate(labels):
-#     if label_dict[label].lower() == labels_keypoint[i]:
-#         keypoint_accuracy += 1
-# keypoint_accuracy = keypoint_accuracy / len(original_images)
-# print(keypoint_accuracy)
 
-####################################
-#       keypoint diagarm
-####################################
-"""
-
-reverse_label_dict = {v: k for k, v in label_dict.items()}
-numeric_labels = np.array([reverse_label_dict[logo.capitalize()] for logo in labels_keypoint])
-
-cm = confusion_matrix(labels, numeric_labels)
-
-class_names = ["Apple", "Honda", "Nike", "Peugeot"]
-disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=class_names)
-disp.plot(cmap=plt.cm.Blues)
-plt.savefig("Confusion_Matrix_Keypoint.png", dpi=300, bbox_inches='tight')
-plt.clf()
-
-class_accuracies = cm.diagonal() / cm.sum(axis=1)
-plt.bar(class_names, class_accuracies, color='skyblue')
-plt.title("Accuracy per Class")
-plt.xlabel("Class")
-plt.ylabel("Accuracy")
-plt.ylim(0, 1)
-plt.xticks(rotation=45)
-for i, acc in enumerate(class_accuracies):
-    plt.text(i, acc + 0.02, f"{acc:.2f}", ha='center')
-
-plt.savefig("Accuracy_per_Class_Keypoint.png", dpi=300, bbox_inches='tight')
-
-"""
 processed_images = processed_images.reshape(-1, 512, 512, 3)
 
 # Adatok és címkék összekeverése
 processed_images, labels = shuffle(processed_images, labels, random_state=42)
 
 X_train, X_test, y_train, y_test, image_paths_train, image_paths_test = train_test_split(
-    processed_images, labels, image_paths, test_size=0.2, random_state=42, stratify=labels
+    processed_images, labels, image_paths, test_size=0.4, random_state=42, stratify=labels
 )
 
 # Adatbővítés
@@ -132,10 +85,72 @@ y_test = to_categorical(y_test, num_classes=len(config.LOGO_PATHS_WITH_TEMPLATES
 model = build_model((512, 512, 3))
 loss, accuracy = train_model(model, X_train, y_train, X_test, y_test, epochs=20)
 
-print(f'Teszt veszteség: {loss}')
-print(f'Teszt pontosság: {accuracy}')
+print(f'CNN Teszt veszteség: {loss}')
+print(f'CNN Teszt pontosság: {accuracy}')
 
-# label_dict = { 0: 'Honda', 1: 'Apple', 2: 'Nike', 3: 'Peugeot' }
+def create_plots(predicted_labels, label_dict, cumulated_accuracy, is_cnn):
+    reverse_label_dict = {v: k for k, v in label_dict.items()}
+    if is_cnn:
+        numeric_labels = np.array(predicted_labels)
+    else:
+        numeric_labels = np.array([reverse_label_dict[logo.capitalize()] for logo in predicted_labels])
+
+    cm = confusion_matrix([np.argmax(vec) for vec in y_test] if is_cnn else labels, numeric_labels)
+
+    class_names = ["Apple", "Honda", "Nike", "Peugeot"]
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=class_names)
+    disp.plot(cmap=plt.cm.Blues)
+    type = 'CNN' if is_cnn else 'Keypoint'
+    name = 'Confusion_Matrix_' + type + '.png'
+    disp.ax_.set_title(type + ' confusion matrix')
+    plt.savefig(name, dpi=300, bbox_inches='tight')
+    plt.show()
+    plt.clf()
+
+    class_accuracies = cm.diagonal() / cm.sum(axis=1)
+    plt.bar(class_names, class_accuracies, color='skyblue')
+    name = 'Accuracy_per_Class_' + type + '.png'
+    percentage = f"{cumulated_accuracy * 100:.0f}%"
+    plt.title('Cumulated accuracy: ' + str(percentage) + ' \n' + type + ' accuracy per Class')
+    plt.xlabel("Class")
+    plt.ylabel("Accuracy")
+    plt.ylim(0, 1)
+    plt.xticks(rotation=45)
+    for i, acc in enumerate(class_accuracies):
+        plt.text(i, acc + 0.02, f"{acc:.2f}", ha='center')
+
+    plt.savefig(name, dpi=300, bbox_inches='tight')
+    plt.show()
+    plt.clf()
+
+cnn_predictions = []
+for i in X_test:
+    prediction, predicted_label = predict_image(model, i)
+    cnn_predictions.append(predicted_label)
+
+cnn_accuracy = 0
+for i, label in enumerate(y_test):
+    if np.argmax(label, axis=0) == cnn_predictions[i]:
+        cnn_accuracy += 1
+cnn_accuracy = cnn_accuracy / len(X_test)
+# print(f'CNN predikció pontosság: {cnn_accuracy}')
+
+# Keypoint detection accuracy mérés
+labels_keypoint = []
+for image in original_images:
+    prediction, image_2 = predict_with_keypoint(image)
+    labels_keypoint.append(prediction)
+
+keypoint_accuracy = 0
+for i, label in enumerate(labels):
+    if label_dict[label].lower() == labels_keypoint[i]:
+        keypoint_accuracy += 1
+keypoint_accuracy = keypoint_accuracy / len(original_images)
+# print(f'Kulcspont detektálás pontosság: {keypoint_accuracy}')
+
+
+create_plots(cnn_predictions, label_dict, cnn_accuracy, True)
+create_plots(labels_keypoint, label_dict, keypoint_accuracy, False)
 
 def turn_cv_img_to_gui(cv_image):
     cv_image_rgb = cv.cvtColor(cv_image, cv.COLOR_BGR2RGB)
@@ -184,10 +199,6 @@ window_width = 1400
 window_height = 800
 root.geometry(f"{window_width}x{window_height}")
 
-# Create and place the "Show random image" button
-show_button = tk.Button(root, text="Show random image", command=show_random_image)
-show_button.grid(row=0, column=0, columnspan=2, pady=10)
-
 # Create and place CNN prediction image and label
 cnn_image_label = tk.Label(root)
 cnn_image_label.grid(row=1, column=0, padx=10, pady=10)
@@ -199,5 +210,9 @@ keypoint_image_label = tk.Label(root)
 keypoint_image_label.grid(row=1, column=1, padx=10, pady=10)
 keypoint_result_label = tk.Label(root, text="Keypoint Prediction: ")
 keypoint_result_label.grid(row=2, column=1, padx=10, pady=10)
+
+# Create and place the "Show random image" button
+show_button = tk.Button(root, text="Show random image", command=show_random_image)
+show_button.grid(row=0, column=0, columnspan=3, pady=20)
 
 root.mainloop()
